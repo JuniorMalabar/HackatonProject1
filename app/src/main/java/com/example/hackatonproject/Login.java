8bcdf0fcfa4788dbf7c5b018a80b260985b0e9f4
Daniil-Laptop <dduhovny26@gmail.com>
2019-12-13 23:40:54 +0300

    test

--- app/src/main/java/com/example/hackatonproject/Login.java
+++ app/src/main/java/com/example/hackatonproject/Login.java
@@ -3,15 +3,20 @@ package com.example.hackatonproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private EditText loginText;
+    private EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        loginText = findViewById(R.id.login_login);
+        passwordText = findViewById(R.id.login_password);
     }
 
     public void toRegistration(View view){
@@ -20,5 +25,10 @@ public class Login extends AppCompatActivity {
     }
 
     public void signIn(View view) {
+        String login = loginText.getText().toString();
+        String password = passwordText.getText().toString();
+        if (User.tryToRegistrate(login, password)){
+
+        }
     }
 }
