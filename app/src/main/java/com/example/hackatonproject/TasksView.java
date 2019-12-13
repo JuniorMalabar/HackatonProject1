5f0d5ac9a99de607ef95ceefc429d9f42d596ef2
JuniorMalabar <54184462+JuniorMalabar@users.noreply.github.com>
2019-12-13 22:29:47 +0300

    MAAAAAAAAAAAAAAAAAAAAAAAAAAP

--- app/src/main/java/com/example/hackatonproject/TasksView.java
+++ app/src/main/java/com/example/hackatonproject/TasksView.java
@@ -3,16 +3,12 @@ package com.example.hackatonproject;
 import android.app.ListActivity;
 import android.os.Bundle;
 
-import java.util.ArrayList;
 
 public class TasksView extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_tab);
-
     }
-
-
 }
 
