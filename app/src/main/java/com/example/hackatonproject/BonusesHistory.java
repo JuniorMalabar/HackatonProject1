4074da3c018ab4fd84db96d020ca3d2797e37d3c
Daniil-Laptop <dduhovny26@gmail.com>
2019-12-13 16:56:15 +0300

    test

---
+++ app/src/main/java/com/example/hackatonproject/BonusesHistory.java
@@ -0,0 +1,4 @@
+package com.example.hackatonproject;
+
+public class BonusesHistory {
+}
--- app/src/main/java/com/example/hackatonproject/User.java
+++ app/src/main/java/com/example/hackatonproject/User.java
@@ -1,4 +1,13 @@
 package com.example.hackatonproject;
 
 public class User {
+    public Integer id;
+    public String login;
+    public String password;
+    public Integer points;
+    public BonusesHistory history;
+    public User() {
+
+    }
+    public static boolean is
 }
