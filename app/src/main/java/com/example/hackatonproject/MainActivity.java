18da93a192757353718851a7fd60e663df5b3d36
NeTRuS-dev <53965024+NeTRuS-dev@users.noreply.github.com>
2019-12-13 17:37:33 +0300

    added work with tasks

--- app/src/main/java/com/example/hackatonproject/MainActivity.java
+++ app/src/main/java/com/example/hackatonproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hackatonproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     boolean running = false;
 
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,25 +35,42 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         running = true;
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(countSensor != null){
-            sensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
-        }
-        else {
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
             Toast.makeText(this, "Sensor not found, sorry", Toast.LENGTH_SHORT).show();
         }
     }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        running = false;
+//
+//    } @Override
+//    protected void onStop() {
+//        super.onStop();
+//        running = false;
+//
+//    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        running = false;
-    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-       if(running){
-           textView.setText(String.valueOf(event.values[0]));
-       }
+        if (running) {
+            textView.setText(String.valueOf(event.values[0]));
+
+            SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+            int stepsPassed = sharedPreferences.getInt("stepsGone", -1);
+            if (stepsPassed != -1) {
+                int diff = (int) event.values[0] - stepsPassed;
+                //убавление разницы у тасков
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.putInt("stepsGone", (int) event.values[0]);
+                editor.apply();
+            }
+        }
     }
 
     @Override
