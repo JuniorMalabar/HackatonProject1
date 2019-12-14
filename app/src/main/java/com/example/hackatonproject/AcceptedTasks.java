f3d0729f1dee3517d7987599347e809ecb26255c
JuniorMalabar <54184462+JuniorMalabar@users.noreply.github.com>
2019-12-14 08:54:01 +0300

    add new views + classes

---
+++ app/src/main/java/com/example/hackatonproject/AcceptedTasks.java
@@ -0,0 +1,18 @@
+
+package com.example.hackatonproject;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class AcceptedTasks extends ListActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted_tasks);
+    }
+}
+
+
+
+
--- app/src/main/java/com/example/hackatonproject/AllTasksTab.java
+++ app/src/main/java/com/example/hackatonproject/AllTasksTab.java
@@ -4,11 +4,14 @@ import android.app.ListActivity;
 import android.os.Bundle;
 
 
-public class TasksView extends ListActivity {
+public class AllTasksTab extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_tab);
     }
 }
+
+
+
 
