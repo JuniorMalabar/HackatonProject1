b10744bb24047a866403c4f9e7d8376c83dc64e4
NeTRuS-dev <53965024+NeTRuS-dev@users.noreply.github.com>
2019-12-15 03:15:11 +0300

    auth

--- app/src/main/java/com/example/hackatonproject/GlobalRequest.java
+++ app/src/main/java/com/example/hackatonproject/GlobalRequest.java
@@ -1,35 +1,6 @@
 package com.example.hackatonproject;
 
-import android.view.View;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 
 public class GlobalRequest {
