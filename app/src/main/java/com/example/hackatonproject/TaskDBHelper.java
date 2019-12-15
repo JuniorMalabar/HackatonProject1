570c38ebfbe0852e414c290df11202bedb8e2741
NeTRuS-dev <53965024+NeTRuS-dev@users.noreply.github.com>
2019-12-15 12:18:05 +0300

    fixed daniil's ass

--- app/src/main/java/com/example/hackatonproject/TaskDBHelper.java
+++ app/src/main/java/com/example/hackatonproject/TaskDBHelper.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class TaskDBHelper extends SQLiteOpenHelper {
 
-    public TaskDBHelper(Context context){
+    public TaskDBHelper(Context context) {
         super(context, "TasksDB", null, 1);
     }
 
@@ -32,33 +32,39 @@ public class TaskDBHelper extends SQLiteOpenHelper {
         cv.put("pointsReward", pointsReward);
         cv.put("decision", decision);
         SQLiteDatabase db = getWritableDatabase();
-        return (int)db.insert("tasks", null, cv);
+        // Cursor cur = db.rawQuery("SELECT COUNT(*) WHERE `type`=? and `value`=? and `ratingReward`=? and `pointsReward`=? and `decision`=?", new String[]{type.toString(), value, ratingReward.toString(), pointsReward.toString(), decision.toString()});
+        //  while (!(cur.getCount() > 0)) {
+        int tm = (int) db.insert("tasks", null, cv);
+        //    cur = db.rawQuery("SELECT COUNT(*) WHERE `type`=? and `value`=? and `ratingReward`=? and `pointsReward`=? and `decision`=?", new String[]{type.toString(), value, ratingReward.toString(), pointsReward.toString(), decision.toString()});
+        return tm;
+
+    }
+    // cur.moveToFirst();
 //        db = getReadableDatabase();
 //        Cursor cursor = db.query("tasks", new String[] {"_id"}, "type = ? and value = ? and ratingReward = ? and pointsReward = ? and decision = ?", new String[] {type.toString(), value, ratingReward.toString(), pointsReward.toString(), decision.toString()}, null, null, null);
 //        cursor.moveToFirst();
 //        return cursor.getInt(cursor.getColumnIndex("_id"));
-    }
 
     public void delete(Task task) {
         SQLiteDatabase db = getWritableDatabase();
-        db.delete("tasks", "_id = ?", new String[] {task.id.toString()});
+        db.delete("tasks", "_id = ?", new String[]{task.id.toString()});
     }
 
     public ArrayList<StepsCountTask> getStepsCountTasks() {
         ArrayList<StepsCountTask> tasks = new ArrayList<>();
         Integer type = Task.TASK_TYPE_STEPS_COUNT;
         SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query("tasks", new String[] {"*"}, "type = ?", new String[] {type.toString()}, null, null, null);
-        if (cursor != null){
+        Cursor cursor = db.query("tasks", new String[]{"*"}, "type = ?", new String[]{type.toString()}, null, null, null);
+        if (cursor.getCount()>0) {
             cursor.moveToFirst();
-            while (cursor.moveToNext()){
+            do {
                 int id = cursor.getInt(cursor.getColumnIndex("_id"));
                 String value = cursor.getString(cursor.getColumnIndex("value"));
                 int ratingReward = cursor.getInt(cursor.getColumnIndex("ratingReward"));
                 int pointsReward = cursor.getInt(cursor.getColumnIndex("pointsReward"));
                 boolean decision = cursor.getInt(cursor.getColumnIndex("decision")) == 1;
                 tasks.add(new StepsCountTask(id, value, ratingReward, pointsReward, decision));
-            }
+            } while (cursor.moveToNext());
         }
         return tasks;
     }
@@ -66,12 +72,12 @@ public class TaskDBHelper extends SQLiteOpenHelper {
     public ArrayList<PointsVisitTask> getPointsTasks() {
         ArrayList<PointsVisitTask> tasks = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
-        Integer type1 = Task.TASK_TYPE_GO_TO_POINT;
-        Integer type2 = Task.TASK_TYPE_GO_TO_ROUTE;
-        Cursor cursor = db.query("tasks", new String[] {"*"}, "type = ? or type = ?", new String[] {type1.toString(), type2.toString()}, null, null, null);
-        if (cursor != null){
+        int type1 = Task.TASK_TYPE_GO_TO_POINT;
+        int type2 = Task.TASK_TYPE_GO_TO_ROUTE;
+        Cursor cursor = db.query("tasks", new String[]{"*"}, "type IN (?, ?)", new String[]{Integer.toString(type1), Integer.toString(type2)}, null, null, null);
+        if (cursor.getCount()>0) {
             cursor.moveToFirst();
-            while (cursor.moveToNext()){
+            do {
                 int id = cursor.getInt(cursor.getColumnIndex("_id"));
                 String value = cursor.getString(cursor.getColumnIndex("value"));
                 int type = cursor.getInt(cursor.getColumnIndex("type"));
@@ -79,14 +85,14 @@ public class TaskDBHelper extends SQLiteOpenHelper {
                 int pointsReward = cursor.getInt(cursor.getColumnIndex("pointsReward"));
                 boolean decision = cursor.getInt(cursor.getColumnIndex("decision")) == 1;
                 tasks.add(new PointsVisitTask(id, type, value, ratingReward, pointsReward, decision));
-            }
+            } while (cursor.moveToNext());
         }
         return tasks;
     }
 
     public ArrayList<Task> getAllTasks() {
-        ArrayList<StepsCountTask> stepsCountTasks = new ArrayList<>(getStepsCountTasks());
-        ArrayList<PointsVisitTask> pointsVisitTasks = new ArrayList<>(getPointsTasks());
+        ArrayList<StepsCountTask> stepsCountTasks = (getStepsCountTasks());
+        ArrayList<PointsVisitTask> pointsVisitTasks = (getPointsTasks());
         ArrayList<Task> tasks = new ArrayList<>();
         for (StepsCountTask task : stepsCountTasks) {
             tasks.add(task);
@@ -100,6 +106,6 @@ public class TaskDBHelper extends SQLiteOpenHelper {
     public void recreateTable() {
         SQLiteDatabase db = getWritableDatabase();
         db.execSQL("drop table if exists tasks");
-        db.execSQL("create table tasks (_id integer primary key autoincrement, type integer not null, value text not null, ratingReward integer not null, pointsReward integer not null, decision integer not null);");
+        db.execSQL("create table tasks (`_id` integer primary key autoincrement, `type` integer not null, `value` text not null, `ratingReward` integer not null, `pointsReward` integer not null, `decision` integer not null);");
     }
 }
